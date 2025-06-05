a9f8eace71a712eca149fc053d8bf0278c999e0b
Marcel-Dort <MDO@p-kg.de>
2025-06-05 12:29:04 +0200

    Datenbankänderung und test insert

--- database.py
+++ database.py
@@ -6,17 +6,17 @@ class Database:
   @staticmethod
   def dbconnect():
     return  mysql.connector.connect(
-      host="PKG-MDO",
-      user="marcel",
+      host="localhost",
+      user="adolf",
       password="passwort",
-      database="game"
+      database="ssp_projekt"
     )
 
-  def db_insert(wahl_eigene, wahl_gegner, ergebnis):
+  def db_insert(wahl_eigene, wahl_gegner):
     mydb = Database.dbconnect()
     mycursor = mydb.cursor()
-    sql = "INSERT INTO spiele (wahl_eigene, wahl_gegner, ergebnis) VALUES ( %s, %s, %s)"
-    val = (wahl_eigene, wahl_gegner, ergebnis)
+    sql = "INSERT INTO spiele (wahl_eigene, wahl_gegner) VALUES ( %s, %s)"
+    val = (wahl_eigene, wahl_gegner)
     mycursor.execute(sql, val)
     mydb.commit()
 
